8b61a1aeedc2ce8b760ba706a59a5f502d5e3ced
Aapo Kärki <aapokr@gmail.com>
2024-02-29 19:51:51 +0200

    Add ratio modification and mask applying

--- utils/image_processor.py
+++ utils/image_processor.py
@@ -9,21 +9,104 @@ import glob
 import random
 
 class ImageProcessor:
-    def __init__(self, base_folder, dataset_folder_name):
+    def __init__(self, base_folder, dataset_folder_name, mask_folder_name):
+
+        #Train, validation and test ratios.
+        self.train = 0.6
+        self.validation = 0.20
+        self.test = 0.20
+
+        #Initialize dataset filepaths
         self.base_folder = base_folder
         self.dataset_path = os.path.join(base_folder, '..','data', dataset_folder_name)
-        #self.dataset_files = os.listdir(self.dataset_path)
         self.dataset_files = [f for f in os.listdir(self.dataset_path) if f.endswith('.tif')]
 
-
+        #Initialize train, test and validation paths
         self.train_path = os.path.join(base_folder, '..','data', 'train')
         self.test_path = os.path.join(base_folder, '..','data', 'test')
+        self.validation_path = os.path.join(base_folder, '..','data', 'validation')
 
-        for folder in [self.train_path, self.test_path]:
+        #Create train, test and validation folders if needed
+        for folder in [self.train_path, self.test_path, self.validation_path]:
             os.makedirs(folder, exist_ok=True)
 
+        #Setup train, test and validation filepaths
         self.train_files = os.listdir(self.train_path)
         self.test_files = os.listdir(self.test_path)
+        self.validation_files = os.listdir(self.validation_path)
+
+
+        self.train_image_range = range(0, int(len(self.dataset_files) * self.train) + 1)
+        self.test_image_range = range(self.train_image_range.stop + 1, self.train_image_range.stop + int(len(self.dataset_files) * self.test))
+        self.validation_image_range = range(self.test_image_range.stop + 1, self.test_image_range.stop + int(len(self.dataset_files) * self.validation))
+
+
+
+        #Initialize mask filepath list
+        self.mask_path = os.path.join(base_folder, '..', 'data', mask_folder_name, 'mask', 'testing_mask_dataset')
+        self.mask_files = os.listdir(self.mask_path)
+
+        #Initialize mask ranges based on training, testing and validation ratios
+        self.train_mask_length = int(len(self.mask_files) * self.train)
+        self.test_mask_length = int(len(self.mask_files) * self.test)
+        self.validation_mask_length = int(len(self.mask_files) * self.validation)
+
+
+
+        # Calculate lengths of each category (2000)
+        mask_category_length = len(self.mask_files) // 6
+
+        # Calculate lengths of each segment based on ratios
+        train_category_length = int(mask_category_length * self.train)
+        test_category_length = int(mask_category_length * self.test)
+        validation_variation_length = int(mask_category_length * self.validation)
+
+        # Initialize lists to store segmented mask files
+        train_mask_files = []
+        test_mask_files = []
+        validation_mask_files = []
+
+        # Iterate over each category
+        for i in range(6):
+            # Calculate the starting and ending indices for the current category
+            start_index = i * mask_category_length
+            end_index = (i + 1) * mask_category_length
+
+            # Slice the current category into train, test, and validation segments
+            train_variation = self.mask_files[start_index:start_index + train_category_length]
+            test_variation = self.mask_files[start_index + train_category_length:start_index + train_category_length + test_category_length]
+            validation_variation = self.mask_files[start_index + train_category_length + test_category_length:end_index]
+
+            # Append segmented mask files from the current category to the corresponding lists
+            train_mask_files.extend(train_variation)
+            test_mask_files.extend(test_variation)
+            validation_mask_files.extend(validation_variation)
+
+        # Assign segmented mask files to respective attributes
+        self.train_mask_files = train_mask_files
+        self.test_mask_files = test_mask_files
+        self.validation_mask_files = validation_mask_files
+
+        '''
+        print("Ratios:")
+        print("Train:", self.train)
+        print("Test:", self.test)
+        print("Validation:", self.validation)
+        print()
+
+        print("Number of dataset images:", len(self.dataset_files))
+        print("Dataset image ratios:")
+        print("Train:", self.train_image_range.start + 1, "-",self.train_image_range.stop +1)
+        print("Test:", self.test_image_range.start + 1, "-", self.test_image_range.stop +1)
+        print("Validation:", self.validation_image_range.start + 1, "-", self.validation_image_range.stop + 1)
+        print()
+
+        print("Number of mask images: ", len(self.mask_files))
+        print("Mask image ratios:")
+        print("Train:", self.train_mask_length)
+        print("Test:", self.test_mask_length)
+        print("Validation:", self.validation_mask_length)
+        '''
 
     def random_scaling(self, image):
         """
@@ -39,17 +122,18 @@ class ImageProcessor:
         scaled_image = cv2.resize(image, None, fx=scale_factor, fy=scale_factor)
         return scaled_image
 
-    def horizontal_flip(self, image):
+    def flip(self, image, direction = 1):
         """
-        Performs horizontal flipping of the given image.
+        Performs a flip to the given image.
 
         Parameters:
             image: numpy.ndarray - The input image as a NumPy array.
+            direction: int - The direction of the flip (1 - horizontal, 0 - vertical, -1 - both axes)
 
         Returns:
-            numpy.ndarray - The horizontally flipped image as a NumPy array.
+            numpy.ndarray - The flipped image as a NumPy array.
         """
-        return cv2.flip(image, 1)
+        return cv2.flip(image, direction)
 
     def random_crop(self, image, min_crop, max_crop):
         """
@@ -84,6 +168,9 @@ class ImageProcessor:
 
         return cropped_image
 
+    def apply_mask(self, image, mask):
+        masked_image = image * mask[:, :, np.newaxis]
+        return masked_image
 
     def segment_and_save(self, source_folder, image_filename, output_folder, segment_size=(512, 512)):
         image_path = os.path.join(source_folder, image_filename)
@@ -119,21 +206,54 @@ class ImageProcessor:
                 # Save the segment
                 cv2.imwrite(os.path.join(output_folder, f"{image_name}_{i}_{j}.jpg"), segment)
 
-if __name__ == '__main__':
-    p = ImageProcessor(os.path.dirname(__file__), '2_Ortho_RGB')
 
-    train_range = range(0,18)
-    test_range = range(19,37)
+def clear_folder(folder_path):
+    # Iterate over the files in the folder
+    for file_name in os.listdir(folder_path):
+        # Construct the full path to the file
+        file_path = os.path.join(folder_path, file_name)
+
+        # Check if the path points to a file
+        if os.path.isfile(file_path):
+            # Remove the file
+            os.remove(file_path)
+        # If it's a directory, recursively clear its contents
+        elif os.path.isdir(file_path):
+            clear_folder(file_path)
+            # After clearing the subdirectory, remove it
+            os.rmdir(file_path)
+
+
+'''
+This main function clears and recreates all segmented satellite images to train, validation and test folders
+'''
+if __name__ == '__main__':
+    dataset_folder_name = '2_Ortho_RGB'
+    mask_folder_name = 'mask'
+
+    p = ImageProcessor(os.path.dirname(__file__), dataset_folder_name, mask_folder_name)
+
+    folders = [p.train_path,p.test_path,p.validation_path]
+    for folder in folders:
+        clear_folder(folder)
 
     # Generate the training images
-    for i in train_range:
+    for i in p.train_image_range:
         p.segment_and_save(p.dataset_path, p.dataset_files[i], p.train_path, segment_size=(512, 512))
         break  # REMOVE THIS TO USE THE WHOLE DATASET
 
     # Generate the testing images
-    for i in test_range:
+    for i in p.test_image_range:
         p.segment_and_save(p.dataset_path, p.dataset_files[i], p.test_path, segment_size=(512, 512))
         break  # REMOVE THIS TO USE THE WHOLE DATASET
+
+    # Generate the validation images
+    for i in p.validation_image_range:
+        p.segment_and_save(p.dataset_path, p.dataset_files[i], p.validation_path, segment_size=(512, 512))
+        break  # REMOVE THIS TO USE THE WHOLE DATASET
+
+
+
 
 
 
--- utils/test_image_processing.py
+++ utils/test_image_processing.py
@@ -1,8 +1,14 @@
 import os
-
+import time
+import torch
 import cv2
 from matplotlib import pyplot as plt
 from utils.image_processor import ImageProcessor
+import numpy as np
+from random import randint, seed
+
+def create_random_mask(image, percentage):
+    return None
 
 def compare_images(im1, im2):
     fig, axes = plt.subplots(1, 2, figsize=(10, 5))
@@ -16,23 +22,26 @@ def compare_images(im1, im2):
     plt.show()
 
 if __name__ == '__main__':
-    p = ImageProcessor(os.path.dirname(__file__), '2_Ortho_RGB')
+    p = ImageProcessor(os.path.dirname(__file__), '2_Ortho_RGB', 'all_masks')
 
     # Read a segmented image which to test
-    im1 = cv2.imread(os.path.join(p.train_path, p.train_files[4]))
+    #im1 = cv2.imread(os.path.join(p.train_path, p.train_files[4]))
 
     # Scale the image randomly between 0.5 and 2.0
-    im2 = p.random_scaling(im1)
+    #im2 = p.random_scaling(im1)
 
     # Flip the image horizontally
-    im2 = p.horizontal_flip(im2)
+    #im2 = p.flip(im2)
 
     # Crop the image to a random size with the width and height sizes being the limits
-    im2 = p.random_crop(im2, 100, 512)
+    #im2 = p.random_crop(im2, 100, 512)
+
+    #mask = create_random_mask(im1, 10)
 
     # Compare images side by side
-    compare_images(im1,im2)
+    #compare_images(im1,mask)
 
+    #print(mask)
 
 
 
